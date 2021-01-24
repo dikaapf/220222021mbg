e64de6dab0fc47396168afbce45428dea95f57a3
nurcholisramlan <oliz.ramlan@gmail.com>
2021-01-24 17:52:34 +0700

    Menghapus home-counting/ jumlah pengajaran yang sudah dilakukan di halaman about us

--- application/modules/home/views/about_us.php
+++ application/modules/home/views/about_us.php
@@ -1,10 +1,7 @@
     <!-- Why choose  -->
-    <?php foreach($about_us as $row){
+    <?php foreach ($about_us as $row) {
 
         echo $row->description;
     }
 
-   ?>
-    <!-- Why choose  -->
-        <!-- Ends How-it-works -->
-      <?php $this->load->view('lesson_count.php'); ?>
+    ?>
